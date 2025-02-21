78aa0925f745383ccc806f9aa763d3b82c0214df
Alumno <alumno@max11>
2025-02-21 09:53:12 +0100

    Primer Commit

---
+++ .doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"Sonic"}];updateSearchResults();
---
+++ .doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Sonic","l":"Sonic_Hedgehog"}];updateSearchResults();
