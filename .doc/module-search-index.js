75e55149207ae8ad4e546e07c1ae0c00b00d3eaf
Montestreh <montescruzmario@gmail.com>
2025-02-21 10:08:54 +0100

    Delete .doc directory

--- .doc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"Sonic"}];updateSearchResults();
--- .doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Sonic","l":"Sonic_Hedgehog"}];updateSearchResults();
